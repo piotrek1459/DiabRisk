5a69fe02d08a6ee6eeb62e7e499f90fe6801b477
Jeremi Szczotka <jeremiszczotka7@gmail.com>
2025-11-19 21:44:40 +0100

    training ml

---
+++ src/Ml/main.py
@@ -0,0 +1,38 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report
+from imblearn.over_sampling import SMOTE
+
+# 1. Wczytanie danych
+X_train = pd.read_csv("C:/Users/Jeremi/Desktop/se_project/DiabRisk/data/processed/X_train_processed.csv")
+X_test = pd.read_csv("C:/Users/Jeremi/Desktop/se_project/DiabRisk/data/processed/X_test_processed.csv")
+
+y_train = pd.read_csv("C:/Users/Jeremi/Desktop/se_project/DiabRisk/data/processed/y_train.csv")
+y_test = pd.read_csv("C:/Users/Jeremi/Desktop/se_project/DiabRisk/data/processed/y_test.csv")
+
+y_train = y_train["Diabetes_012"]
+y_test = y_test["Diabetes_012"]
+
+print("Rozkład klas PRZED SMOTE:")
+print(y_train.value_counts())
+
+# 2. SMOTE – automatycznie oversampluje wszystkie mniejszości do poziomu większości
+smote = SMOTE(sampling_strategy="not majority", random_state=42)
+X_train_res, y_train_res = smote.fit_resample(X_train, y_train)
+
+print("\nRozkład klas PO SMOTE:")
+print(y_train_res.value_counts())
+
+# 3. Model
+model = RandomForestClassifier(
+    n_estimators=200,
+    n_jobs=-1,
+    random_state=42
+)
+
+model.fit(X_train_res, y_train_res)
+
+# 4. Ewaluacja
+y_pred = model.predict(X_test)
+print("\n=== Classification report (po SMOTE) ===")
+print(classification_report(y_test, y_pred))
