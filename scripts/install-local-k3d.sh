c4a374dd53c47f3989f0b2474d016df8d9ce0e18
Piotr Krupiński <piotrek1459@gmail.com>
2026-01-10 14:42:05 +0100

    PoC works

--- scripts/install-local-k3d.sh
+++ scripts/install-local-k3d.sh
@@ -3,6 +3,19 @@ set -euo pipefail
 
 CLUSTER_NAME="${1:-diabrisk}"
 
+echo "=== Checking k3d cluster '$CLUSTER_NAME' ==="
+
+if k3d cluster list | grep -q "^$CLUSTER_NAME "; then
+  echo "Cluster '$CLUSTER_NAME' already exists."
+  k3d cluster delete $CLUSTER_NAME
+fi
+echo "Creating k3d cluster '$CLUSTER_NAME' with port 80 mapped..."
+k3d cluster create "$CLUSTER_NAME" \
+  --api-port 6443 \
+  -p "80:80@loadbalancer"
+echo "Cluster created successfully!"
+
+
 echo "=== Building Docker images ==="
 
 # Backend
